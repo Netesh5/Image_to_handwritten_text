e58268ec272a1834f85a3caa2f26ae61173795f2
Netesh5 <netesh.paudel@gmail.com>
2022-05-03 22:38:53 +0545

    google sign out

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,5 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:imagetotext/firebaseServices/AuthService/auth.dart';
 import 'package:imagetotext/screens/homepage.dart';
 import 'package:imagetotext/screens/loginScreen.dart';
 import 'package:provider/provider.dart';
@@ -9,7 +9,7 @@ class wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<Auth>(context, listen: false);
+    final user = Provider.of<User?>(context);
     if (user == null) {
       return const loginScreen();
     } else {
