c1db840b61a6b122af532946177add6a0bb2c9fa
Netesh5 <netesh.paudel@gmail.com>
2022-05-04 21:41:23 +0545

    drawer items

---
+++ lib/constants/loadingIndicator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class loading with ChangeNotifier {
+  bool isLoading = false;
+  Widget loadingIndicator(context) {
+    return const CircularProgressIndicator(
+      color: Colors.white,
+    );
+  }
+}
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,27 +1,67 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:imagetotext/firebaseServices/AuthService/auth.dart';
 import 'package:provider/provider.dart';
 
-Widget drawer() {
+Widget drawer(context) {
   Auth auth = Auth();
+  final user = Provider.of<User>(context, listen: false);
   return Drawer(
     child: SafeArea(
-      child: Column(
-        children: [
-          Consumer<Auth>(
-            builder: (context, googleSignout, child) => GestureDetector(
-              onTap: () async {
-                await auth.googleSignOut(context);
-                debugPrint("logged out");
-              },
-              child: const ListTile(
-                tileColor: Colors.indigoAccent,
-                title: Text("Log out"),
+      child: Column(children: [
+        Container(
+          height: 70,
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 10,
               ),
+              CircleAvatar(
+                backgroundImage: NetworkImage(user.photoURL!),
+                radius: 35,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(user.displayName!)
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Divider(
+          color: Colors.black,
+        ),
+        const ListTile(
+          leading: Icon(Icons.home),
+          title: Text("Home"),
+        ),
+        const ListTile(
+          leading: Icon(Icons.feedback_outlined),
+          title: Text("Feedback"),
+        ),
+        const ListTile(
+          leading: Icon(Icons.star_outline_sharp),
+          title: Text("Rate us"),
+        ),
+        const ListTile(
+          leading: Icon(Icons.contact_phone),
+          title: Text("Contact us"),
+        ),
+        Consumer<Auth>(
+          builder: (context, googleSignout, child) => GestureDetector(
+            onTap: () async {
+              await auth.googleSignOut(context);
+              debugPrint("logged out");
+            },
+            child: const ListTile(
+              leading: Icon(Icons.logout),
+              title: Text("Log out"),
             ),
           ),
-        ],
-      ),
+        ),
+      ]),
     ),
   );
 }
