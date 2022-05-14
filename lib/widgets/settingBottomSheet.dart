98fcae1d3e20ce769e159893d900aa453896a352
Netesh5 <netesh.paudel@gmail.com>
2022-05-14 16:24:41 +0545

    word spacing settings

--- lib/widgets/settingBottomSheet.dart
+++ lib/widgets/settingBottomSheet.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 
 bottomSheet(context) {
   Object? currentIndex;
+  double currentValue = 5;
   return showModalBottomSheet(
       context: context,
       backgroundColor: Theme.of(context).backgroundColor,
@@ -20,104 +21,216 @@ bottomSheet(context) {
                 topRight: Radius.circular(20.0),
               ),
             ),
-            child: Column(
-              children: [
-                ListTile(
-                  title: Center(
-                    child: Text(
-                      "Settings",
-                      style: TextStyle(
-                          fontSize:
-                              Theme.of(context).textTheme.headline6!.fontSize),
-                    ),
-                  ),
-                  leading: GestureDetector(
-                    child: const Icon(Icons.close),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  trailing: GestureDetector(
-                    child: const Icon(Icons.done),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(),
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                        child: Column(
-                          children: [
-                            const Text(
-                              "Choose font",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 16),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.9,
-                              child: Provider.of<dropDownMenu>(context)
-                                  .dropdownButtonFormField(context),
-                              decoration: BoxDecoration(
-                                  border: Border.all(),
-                                  borderRadius: BorderRadius.circular(20)),
-                            )
-                          ],
-                        ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Center(
+                      child: Text(
+                        "Settings",
+                        style: TextStyle(
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .fontSize),
                       ),
                     ),
-                    const SizedBox(
-                      height: 20,
+                    leading: GestureDetector(
+                      child: const Icon(Icons.close),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(),
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                        child: Column(
-                          children: [
-                            const Text(
-                              "Font size",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 16),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.9,
-                              child: TextFormField(
-                                textAlign: TextAlign.center,
-                                textInputAction: TextInputAction.next,
-                                initialValue: "20",
-                                keyboardType: TextInputType.number,
-                                decoration: const InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "      Enter font size"),
+                    trailing: GestureDetector(
+                      child: const Icon(Icons.done),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                          child: Column(
+                            children: [
+                              const Text(
+                                "Choose font",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
                               ),
-                              decoration: BoxDecoration(
-                                  border: Border.all(),
-                                  borderRadius: BorderRadius.circular(20)),
-                            )
-                          ],
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: Provider.of<dropDownMenu>(context)
+                                    .dropdownButtonFormField(context),
+                                decoration: BoxDecoration(
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.circular(20)),
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                )
-              ],
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                          child: Column(
+                            children: [
+                              const Text(
+                                "Font size",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: TextFormField(
+                                  textAlign: TextAlign.center,
+                                  textInputAction: TextInputAction.next,
+                                  initialValue: "20",
+                                  keyboardType: TextInputType.number,
+                                  decoration: const InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "      Enter font size"),
+                                ),
+                                decoration: BoxDecoration(
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.circular(20)),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                          child: Column(
+                            children: [
+                              const Text(
+                                "Height gap",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: Slider(
+                                    min: 0,
+                                    max: 100,
+                                    value: currentValue,
+                                    onChanged: (value) {
+                                      currentValue = value;
+                                    }),
+                                decoration: BoxDecoration(
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.circular(20)),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                          child: Column(
+                            children: [
+                              const Text(
+                                "Word Spacing",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: Slider(
+                                    min: 0,
+                                    max: 100,
+                                    value: currentValue,
+                                    onChanged: (value) {
+                                      currentValue = value;
+                                    }),
+                                decoration: BoxDecoration(
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.circular(20)),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+                          child: Column(
+                            children: [
+                              const Text(
+                                "Letter spacing",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.9,
+                                child: Slider(
+                                    min: 0,
+                                    max: 100,
+                                    value: currentValue,
+                                    onChanged: (value) {
+                                      currentValue = value;
+                                    }),
+                                decoration: BoxDecoration(
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.circular(20)),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ));
 }
